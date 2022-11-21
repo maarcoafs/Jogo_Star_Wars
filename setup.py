083c873bea11651a6ae6e3e985df7d77ef07806f
Marco Flores <maarco.afs@gmail.com>
2022-11-21 15:06:22 -0300

    V0.4 - Configurado Setup

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 import cx_Freeze
 
-executables = [cx_Freeze.Executable(
-    script="IronMan.py",
-    icon="assets/ironIcon.ico"
-)]
-cx_Freeze.setup(
-    name="IronMan",
-    options={
-        "build_exe":{"packages":["pygame"],
-        "include_files":["assets"]
-        }}
-    ,executables = executables
-)
+executables = [cx_Freeze.Executable(script="StarWars.py",icon="assets/StarWarsIcon.ico")]
 
+cx_Freeze.setup(
+ name="Jogo Star Wars",
+ options={"build_exe": {"packages":["pygame"],
+ "include_files":["assets"]}},
+ executables = executables
+ ) 
